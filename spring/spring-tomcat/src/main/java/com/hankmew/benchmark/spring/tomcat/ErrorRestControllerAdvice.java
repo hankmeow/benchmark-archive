80a36088628c52a1130c2ff2849b2b554c02193c
hankmew <hankmew@gmail.com>
2021-01-19 11:38:13 +0800

    404

--- spring/spring-tomcat/src/main/java/com/hankmew/benchmark/spring/tomcat/ErrorRestControllerAdvice.java
+++ spring/spring-tomcat/src/main/java/com/hankmew/benchmark/spring/tomcat/ErrorRestControllerAdvice.java
@@ -3,18 +3,23 @@ package com.hankmew.benchmark.spring.tomcat;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
-@ResponseBody
 @RestControllerAdvice
-public class RestErrorController {
+public class ErrorRestControllerAdvice {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Object handleError(Exception e) {
         return "error";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Object handleNotFound(NoHandlerFoundException e) {
+        return "404";
     }
 }
