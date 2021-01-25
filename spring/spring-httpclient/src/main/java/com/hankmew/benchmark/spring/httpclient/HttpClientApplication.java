d79f3eb97bf702d8ab946de142b3897e1f50160d
hankmew <hankmew@gmail.com>
2021-01-25 16:13:30 +0800

    httpclient

---
+++ spring/spring-httpclient/src/main/java/com/hankmew/benchmark/spring/httpclient/HttpClientApplication.java
@@ -0,0 +1,7 @@
+package com.hankmew.benchmark.spring.httpclient;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpClientApplication {
+}
