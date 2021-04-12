c05df6d7f6b7c0c5fff4e115a89f95774a996993
hankmew <hankmew@gmail.com>
2021-04-12 22:49:49 +0800

    增加定时任务

--- spring/spring-threadpool/src/main/java/com/hankmew/benchmark/spring/threadpool/AsyncThreadPoolConfig.java
+++ spring/spring-threadpool/src/main/java/com/hankmew/benchmark/spring/threadpool/AsyncThreadPoolConfig.java
@@ -1,23 +1,37 @@
 package com.hankmew.benchmark.spring.threadpool;
-import java.util.concurrent.RejectedExecutionHandler;
-import org.springframework.core.task.TaskDecorator;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
-@EnableAsync
 public class AsyncThreadPoolConfig {
-    //todo 如何优雅关闭
+
+    @Bean("defaultTaskScheduler")
+    public ThreadPoolTaskScheduler defaultTaskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setThreadNamePrefix("defaultTaskScheduler-");
+        //该方法用来设置 线程池关闭 的时候 等待 所有任务都完成后，再继续 销毁 其他的 Bean，这样这些 异步任务 的 销毁 就会先于 数据库连接池对象 的销毁。
+        taskScheduler.setWaitForTasksToCompleteOnShutdown(true);
+        //该方法用来设置线程池中 任务的等待时间，如果超过这个时间还没有销毁就 强制销毁，以确保应用最后能够被关闭，而不是阻塞住。
+        taskScheduler.setAwaitTerminationSeconds(60);
+        taskScheduler.setPoolSize(4);
+        taskScheduler.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+        return taskScheduler;
+    }
+
     @Bean("defaultTaskExecutor")
-    public TaskExecutor defaultThreadPoolTaskExecutor() {
+    public TaskExecutor defaultTaskExecutor() {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setThreadGroupName("default");
+        taskExecutor.setThreadGroupName("defaultTaskExecutor-");
+        //该方法用来设置 线程池关闭 的时候 等待 所有任务都完成后，再继续 销毁 其他的 Bean，这样这些 异步任务 的 销毁 就会先于 数据库连接池对象 的销毁。
+        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        //该方法用来设置线程池中 任务的等待时间，如果超过这个时间还没有销毁就 强制销毁，以确保应用最后能够被关闭，而不是阻塞住。
+        taskExecutor.setAwaitTerminationSeconds(60);
         taskExecutor.setCorePoolSize(1);
         taskExecutor.setMaxPoolSize(1);
         taskExecutor.setKeepAliveSeconds(60);
@@ -25,5 +39,5 @@ public class AsyncThreadPoolConfig {
         taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
         return taskExecutor;
     }
-    //todo 定时任务线程
+
 }
--- spring/spring-threadpool/src/main/java/com/hankmew/benchmark/spring/threadpool/ThreadPoolApplication.java
+++ spring/spring-threadpool/src/main/java/com/hankmew/benchmark/spring/threadpool/ThreadPoolApplication.java
@@ -2,12 +2,33 @@ package com.hankmew.benchmark.spring.threadpool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class ThreadPoolApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThreadPoolApplication.class, args);
+    }
+
+    /**
+     * Async注解会在以下几个场景失效，也就是说明明使用了@Async注解，但就没有走多线程。
+     *
+     * 异步方法使用static关键词修饰；
+     * 异步类不是一个Spring容器的bean（一般使用注解@Component和@Service，并且能被Spring扫描到）；
+     * SpringBoot应用中没有添加@EnableAsync注解；
+     * 在同一个类中，一个方法调用另外一个有@Async注解的方法，注解不会生效。原因是@Async注解的方法，是在代理类中执行的。
+     * 需要注意的是： 异步方法使用注解@Async的返回值只能为void或者Future及其子类，当返回结果为其他类型时，方法还是会异步执行，但是返回值都是null
+     * @throws InterruptedException
+     */
+    @Async(value = "defaultTaskScheduler")
+    @Scheduled(cron = "0/5 * * * * *")
+    public void run() throws InterruptedException {
+        //Thread.sleep(3000);
+        System.out.println(Thread.currentThread().getName()+"=====>>>>>5秒使用cron  {}"+(System.currentTimeMillis()/1000));
     }
 }
