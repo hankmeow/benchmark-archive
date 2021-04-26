a2b550acc8dd2e725cef2833bae9cb2ca70a41d5
hankmew <hankmew@gmail.com>
2021-04-26 10:57:15 +0800

    json

--- spring/spring-tomcat/src/main/java/com/hankmew/benchmark/spring/tomcat/WebConfig.java
+++ spring/spring-tomcat/src/main/java/com/hankmew/benchmark/spring/tomcat/WebConfig.java
@@ -1,9 +1,18 @@
 package com.hankmew.benchmark.spring.tomcat;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.IOException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
@@ -14,5 +23,19 @@ public class WebConfig implements WebMvcConfigurer {
             .allowCredentials(true)
             .allowedHeaders("*")
             .maxAge(3600);
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter () {
+        //重写Json解析器，这里可以获得未序列化前的返回值，还可以自定义返回的一些行为。
+        //最好写成子类实现的形式，便于共享。
+        return new MappingJackson2HttpMessageConverter () {
+            @Override
+            protected void writeInternal (Object object,
+                                          HttpOutputMessage outputMessage) throws IOException,
+                                                                                  HttpMessageNotWritableException {
+                super.writeInternal(object, outputMessage);
+            }
+        };
     }
 }
